ce13d8f93f70d60eb0c1bbb6498777ff5c99cec5
Caleb Tracey <caleb.tracey@gmail.com>
2023-02-04 00:32:47 -0500

    working state for image generation

--- cmd/svr/main.go
+++ cmd/svr/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/calebtracey/ai-interaction-api/internal"
 	config "github.com/calebtracey/config-yaml"
 	"github.com/joho/godotenv"
-	"log"
+	log "github.com/sirupsen/logrus"
 )
 
 const configPath = "config.yaml"
@@ -30,6 +30,8 @@ func (b *builder) initializeDAO() (*internal.DAO, error) {
 }
 
 func main() {
+	defer panicQuit()
+
 	if svc, err := new(builder).initializeDAO(); err != nil {
 		log.Fatal(err)
 	} else {
@@ -41,4 +43,11 @@ func run(svc *internal.DAO) {
 	handler := internal.Handler{DAO: svc}
 
 	log.Fatal(listenAndServe("8080", gziphandler.GzipHandler(corsHandler().Handler(handler.InitializeRoutes()))))
+}
+
+func panicQuit() {
+	if r := recover(); r != nil {
+		log.Errorf("I panicked and am quitting: %v", r)
+		log.Error("I should be alerting someone...")
+	}
 }
